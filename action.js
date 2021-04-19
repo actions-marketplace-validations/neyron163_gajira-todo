58e2f1cbf1036ac57f1118abfcc208a3fb56577b
neyron163 <andrey163783@gmail.com>
2021-04-19 11:23:17 +0400

    replaced regexp for TODO

--- action.js
+++ action.js
@@ -111,7 +111,7 @@ module.exports = class {
   async findTodoInCommits (repo, commits) {
     return Promise.all(commits.map(async (c) => {
       const res = await this.GitHub.getCommitDiff(repo.full_name, c.id)
-      const rx = /^\+.*(?:\/\/|#)\s+TODO:(.*)$/gm
+      const rx = /(\/\/|#|\*)\s+@todo/gmi
 
       return getMatches(res, rx, 1)
         .map(_.trim)
