725e1301e3a4d76c1d4b4b9dea7b6cb206cd6d8f
neyron163 <andrey163783@gmail.com>
2021-04-19 17:36:29 +0400

    added console log

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const fs = require('fs')
 const YAML = require('yaml')
 const core = require('@actions/core')
 
-const cliConfigPath = `${process.env.HOME}/.jira.d/config.yml`
 const configPath = `${process.env.HOME}/jira/config.yml`
 const Action = require('./action')
 const githubToken = process.env.GITHUB_TOKEN
-
+console.log(process.env, ' process.env')
 // eslint-disable-next-line import/no-dynamic-require
 const githubEvent = require(process.env.GITHUB_EVENT_PATH)
 const config = YAML.parse(fs.readFileSync(configPath, 'utf8'))
-
 async function exec () {
   try {
     const result = await new Action({
